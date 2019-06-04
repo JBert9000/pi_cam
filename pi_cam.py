68d95b0e2faf2ddb7af1f6ce70de8260f31bcd7b
John <jbertlik@gmail.com>
2019-06-03 23:02:26 -0400

    added a status variable that has a switch of 0 or 1. I linked this variable to 0 when nothing is moving and 1 to when something big enough that goes in view of the camera to 1. I put these values in time in a list and used datetime to let me know when they change back and forth. I will add these values to a .csv file.

--- pi_cam.py
+++ pi_cam.py
@@ -1,12 +1,14 @@
 import cv2, time
+from datetime import datetime
 
 first_frame=None
-
+status_list=[None,None]
+times=[]
 video=cv2.VideoCapture(0)
 
 while True:
     check, frame=video.read()
-
+    status=0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
 
@@ -22,12 +24,18 @@ while True:
     cnts=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status=1
 
         (x,y,w,h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y),(x+w, y+h), (0,255,0),3)
 
+    status_list.append(status)
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
 
     cv2.imshow("Gray1",gray)
     cv2.imshow("Delta Frame",delta_frame)
@@ -35,11 +43,17 @@ while True:
     cv2.imshow("Colour Frame", frame)
 
     key=cv2.waitKey(1)
-    print(frame)
-    print(delta_frame)
+    # print(frame)
+    # print(delta_frame)
 
     if key==ord('q'):
+        if status==1:
+            times.append(datetime.now())
         break
+
+print(status_list)
+print(times)
+
 
 video.release()
 cv2.destroyAllWindows()
